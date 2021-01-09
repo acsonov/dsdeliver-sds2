1d552e120ae6547ba4f21bff9a4ba4f470791abc
Hugo Acsonov <acsonov@gmail.com>
2021-01-09 00:18:18 -0300

    Fim aula 2

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,17 +1,16 @@
 import './styles.css';
 import { ReactComponent as Logo } from './logo.svg';
-import Home from '../Home';
-import { Link } from 'react-router-dom';
+
 
 
 function Navbar() {
     return (
         <nav className="main-navbar">
             <Logo />
-            <Link to="/" className="logo-text">Ds Deliver</Link>
-            <Home />
+            <a href="home" className="logo-text">
+            </a>
         </nav>
-    );
+    )
 }
 
 export default Navbar;
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,26 @@
+import './styles.css'; 
 import StepsHeader from './StepsHeader';
-import './styles.css';
+import ProductList from './ProductList';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
-function Orders() {
-    return (
+
+function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+
+    useEffect(() =>{
+        fetchProducts().then(response => setProducts(response.data)).catch(error => console.log(error));
+
+    }, []);
+
+    return(
         <div className="orders-container">
             <StepsHeader />
+            <ProductList products = {products}/>
         </div>
     );
 }
-
 export default Orders;
